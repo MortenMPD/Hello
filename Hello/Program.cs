17758e003bb63d1fe3a2bbffd662831d9fc23452
MPD <MPD@DK01WS1691>
2017-06-20 09:03:56 +0200

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello {
+    class Program {
+        static void Main(string[] args)
+        {
+            var text = System.Console.ReadLine();
+            var i = 1;
+            while (i < 1000)
+            {
+                System.Console.WriteLine(text);
+                i++;
+            }
+        }
+    }
+    }
+
